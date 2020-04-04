e01bc16426c9ec2a3ee7c1188a3df1ab38fe314f
nalayaks <sbh.mech@gmail.com>
2020-04-05 03:05:54 +0530

    new codes

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-DATE_TIME = "date/time"
-DATA_URL = (
-    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
-)
 
-st.title("Uber Pickups in New York City")
+st.title("Image Enhancement")
 st.markdown(
 """
 This is a demo of a Streamlit app that shows the Uber pickups
