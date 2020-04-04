a8da2cb922f212a202eb4afa3e87e3628308814a
nalayaks <sbh.mech@gmail.com>
2020-04-05 02:44:50 +0530

    ptch

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-DATE_TIME = "date/time"
-DATA_URL = (
-    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
-)
 
 st.title("Uber Pickups in New York City")
 st.markdown(
