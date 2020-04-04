f6ac29f53257c2e368dfc2b0dcddaaa6510b52a0
nalayaks <sbh.mech@gmail.com>
2020-04-05 03:23:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,34 @@ from PIL import Image
 
 st.title("Image Enhancement")
 
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from keras.applications.vgg16 import VGG16
+
+def predict(image1): 
+    model = VGG16()
+    image = load_img(image1, target_size=(224, 224))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    image = preprocess_input(image)
+    # predict the probability across all output classes
+    yhat = model.predict(image)
+    # convert the probabilities to class labels
+    label = decode_predictions(yhat)
+    # retrieve the most likely result, e.g. highest probability
+    label = label[0][0]
+    return label 
+
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
+    label = predict(uploaded_file)
+    st.write('%s (%.2f%%)' % (label[1], label[2]*100))
