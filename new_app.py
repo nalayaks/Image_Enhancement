c543e7b2b4ad95f5ecf2b78bff56365e4e97474b
nalayaks <sbh.mech@gmail.com>
2020-04-07 01:19:40 +0530

    Update and rename new_app to new_app.py

---
+++ new_app.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import streamlit as st 
+from PIL import Image
+import keras
+
+from ISR.models import RRDN
+rrdn = RRDN(weights='gans')
+
+
+st.title("Image Enhancement")
+
+
+
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    sr_img_gan = rrdn.predict(lr_img)
+    predicted=Image.fromarray(sr_img_gan)
+    st.image(predicted, caption='Enhanced Image.', use_column_width=True)
